aaf3aec24d8b009137857747669f9c4d336100b4
Renan <rgmelo94@gmail.com>
2017-12-31 15:42:11 -0200

    create interface

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+  <my-card> </my-card>
+  </div>
+</template>
+
+<script>
+import Card from './assets/components/card/Card'
+export default {
+  components: {
+    'myCard': Card,
+  },
+  name: 'app',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/assets/components/card/Card.vue
@@ -0,0 +1,104 @@
+<template>
+  <section class="card">
+    <div class="card-titulo">
+      <h3> Card Title </h3>
+    </div>
+    <div class="card-subtitle">
+      <p> this is a subtitle </p>
+    </div>
+    <hr>
+    <div class="card-content">
+      <div class="wrapper-content">
+        <img src="/src/assets/img/morty.png"> </img>
+        <p> oi </p>
+      </div>
+    </div>
+    <div class="card-rodape">
+      <p> this is a rodape </p>
+    </div>
+  </section>
+</template>
+
+<script>
+  export default {
+    name: 'myCard',
+    data(){
+       return {
+
+      }
+    },
+  }
+</script>
+
+<style scoped>
+  * {
+        font-family: Verdana,sans-serif;
+    font-size: 1rem;
+    line-height: 1.5;
+  }
+  .card-titulo h3{
+    font-size: 1.5rem;
+    margin: auto;
+    line-height: 2.5;
+    padding-left: 1rem;
+    font-family: "Segoe UI",Arial,sans-serif;
+    font-weight: 400;
+  }
+  .card{
+    box-shadow: 0 4px 10px 0 rgba(0,0,0,0.2), 0 4px 20px 0 rgba(0,0,0,0.19);
+    width: 50%;
+  }
+  .card-titulo{
+    width:100%;
+    background-color: #e0e0e0;
+    min-height: 3rem;
+  }
+  .card-subtitle{
+   }
+  .card-subtitle p{
+    font-family: Verdana,sans-serif;
+    font-size: 1rem;
+    line-height:1.5;
+    padding-left: 1rem;
+  }
+  .card-content{
+
+  }
+  .card-content img {
+    width: 11%;
+    border-radius: 55px;
+    float: left;
+  }
+  .card-content p {
+    float: left;
+    padding-left: 1rem;
+  }
+  .wrapper-content{
+    min-height: 5rem;
+    height:100%;
+    width:95%;
+    margin: auto;
+  }
+  .card-rodape{
+    background-color:#616161;
+    min-height:  2rem;
+    margin-top: 1.3rem
+  }
+
+  .card-rodape p {
+    text-align: center;
+    color: white;
+    line-height: 2;
+  }
+  .card-rodape:hover{
+    background-color: #bfbfbf;
+    cursor:pointer;
+  }
+
+
+  hr {
+    border: 0;
+    border-top: 1px solid #eee;
+    margin: 20px 0;
+  }
+</style>
